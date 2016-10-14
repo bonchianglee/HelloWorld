e067b721e5700b49022b1ec3f337012309f3270b
bonchiang <leebc-wa15@student.tarc.edu.my>
2016-10-14 12:01:09 +0800

    master copy

---
+++ app/src/main/java/net/soonhe/myhello/MainActivity.java
@@ -0,0 +1,36 @@
+package net.soonhe.myhello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textViewMessage;
+    ImageView imageViewApplePen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Show UI
+        //R=resource
+        setContentView(R.layout.activity_main);
+
+        //Link UI to prgram
+        textViewMessage = (TextView)findViewById(R.id.TextViewMessage);
+        imageViewApplePen = (ImageView)findViewById(R.id.imageViewApplePen);
+    }
+
+    public void showMessage(View view){
+        textViewMessage.setText(getText(R.string.message));
+    }
+
+    public void toggleImage(View view){
+        if(imageViewApplePen.getVisibility()==View.VISIBLE)
+            imageViewApplePen.setVisibility(View.INVISIBLE);
+        else
+            imageViewApplePen.setVisibility(View.VISIBLE);
+    }
+}
